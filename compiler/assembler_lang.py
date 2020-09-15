32136929a8005aab8b15571a2966ae81fde7ff52
Ilya Darkovsky <darkovsky2@gmail.com>
2020-09-15 12:05:17 +0300

    Added more commands and comments

--- compiler/assembler_lang.py
+++ compiler/assembler_lang.py
@@ -19,22 +19,34 @@ class Assembler():
         * Using stack machine for execution of code
 
     Fon Neiman architecture:
-        Using variable self.__mem as common memory space
-        for comands and data.
+        All memory space there are in self.__mem, but logical
+        separating on two parts: for commands and data.
+        Memory ratio for commands and data by default -- 1:3
+        Addresses for commands: [0, FIRST_DATA_ADDRESS - 1]
+        Addresses for data: [FIRST_DATA_ADDRESS: 2**ADDRESS_LENGTH-1]
 
-
-
-    Inputting RISC comands format:
+    Inputting RISC commands format:
         X1_X2_X3_X4, 35 bits length
         [0-7]   X1 -- code of command (see constants.py)
-        [8-23]  X2 -- literal (Filling zeros if not exists)
-        [24-31] X3 -- address (Filling zeros if not exists)
-        [32-34] X4 -- number of register
+        [8-23]  X2 -- literal (Filling zeros if not using)
+        [24-31] X3 -- address (Filling zeros if not using)
+        [32-34] X4 -- number of register (Filling zeros if not using)
     
     Stack machine:
-        
+        Using stack for execution all opertions.
+        3|   | 
+        2|   |
+        1| - |
+        0|_A_| <<-- Stack Pointer
     """
     def __init__(self, compiled_cmds, jumps):
+        # Bynary program and jumps addresses from compiler
+        self.compiled_cmds = compiled_cmds
+        self.jumps = jumps
+        # List of stack, where will be executing all operations.
+        self.__stack = [0, 0, 0, 0, 0]
+        # List, witch using as memory space for commands and operands.
+        self.__mem = np.zeros(2**ADDRESS_LENGTH, dtype=int)
         # Dictionary of common registers[from 1 to 2**REGISTER_LENGTH-1]
         # and also of system registers -- PC and SP
         self.__R = { 
@@ -43,30 +55,26 @@ class Assembler():
         self.__R.update(
             { 
                 'PC': 0, # Programm counter.
-                'SP': 0, # Stack pointer.
+                'SP': 0, # Stack pointer
             }
         )
-        self.__flags = { # Dictionary of flags.
+        self.__flags = {# Dictionary of flags.
             'Z': False, # Zero
             'S': False, # Sign 
             'P': False, # Parity
             'C': False, # Carry
             'O': False, # Overflow
-        } 
-        # List of stack, where will be executing all operations.
-        self.__stack = [0, 0, 0, 0, 0]
-        # List, witch using as memory space for commands and operands.
-        self.__mem = np.zeros(2**ADDRESS_LENGTH, dtype=int)
-        self.compiled_cmds = compiled_cmds
-        self.jumps = jumps
+        }
+
     
     def input_program(self, compiled_cmds, jumps):
         self.compiled_cmds = compiled_cmds
         self.jumps = jumps
     
     def execute_code(self):
-        for cmd in self.compiled_cmds: 
-            # TODO: hardcode
+        while self.__R['PC'] <= len(self.compiled_cmds):
+            cmd = self.compiled_cmds[self.__R['PC']]
+
             cmd_code = int(cmd[:CMDCODE_LENGTH], 2)
             literal  = int(cmd[CMDCODE_LENGTH:CMDCODE_LENGTH+LITERAL_LENGTH], 2)
             address  = int(cmd[CMDCODE_LENGTH+LITERAL_LENGTH:-REGISTER_LENGTH], 2)
@@ -109,6 +117,12 @@ class Assembler():
                 self.__jc(address=address)
             elif cmd_code == 15:
                 self.__jz(address=address)
+            elif cmd_code == 16:
+                self.__jp(address=address)
+            elif cmd_code == 17:
+                self.__js(address=address)
+            elif cmd_code == 18:
+                self.__jo(address=address)
 
     def __cmd_stack_push(self, el):
         self.__stack[self.__R['SP']] = el
@@ -119,6 +133,9 @@ class Assembler():
         return self.__stack[self.__R['SP']]
 
     def __update_flags(self, res):
+        """
+        Updating flags depends of res
+        """
         if res == 0:
             self.__flags['Z'] = True
         else:
@@ -136,6 +153,10 @@ class Assembler():
 
 
     def __add(self):
+        """
+        Addition of the last elements of the stack.
+        Result also pushing to the stack.
+        """        
         op1 = self.__cmd_stack_pop()
         op2 = self.__cmd_stack_pop()
         res = int(str(op1), 0) + int(str(op2), 0)
@@ -147,6 +168,10 @@ class Assembler():
                 )
     
     def __sub(self):
+        """
+        Subtraction of the last elements of the stack.
+        Result also pushing to the stack.
+        """
         res = self.__cmp()
         self.__push(
             literal=res,
@@ -155,6 +180,9 @@ class Assembler():
         )
 
     def __push(self, literal, address, register):
+        """
+        Pushing element to stack as literal or from register or from memory
+        """
         if address == register == 0:
             self.__cmd_stack_push(literal)
         elif literal == register == 0:
@@ -166,6 +194,9 @@ class Assembler():
         self.__R['PC'] += 1
         
     def __pop(self, address, register):
+        """
+        Moving last element of stack to register or to memory
+        """
         if register == 0:
             self.__mem[address] = self.__cmd_stack_pop()
         elif address == 0:
@@ -173,13 +204,19 @@ class Assembler():
         self.__R['PC'] += 1
     
     def __cmp(self):
+        """
+        Comparing last two numbers on stack
+        """
         op1 = self.__cmd_stack_pop()
         op2 = self.__cmd_stack_pop()
-        res = op1 - op2
+        res = op2 - op1
         self.__update_flags(res)
-        return res
+        return abs(res)
     
     def __not(self):
+        """
+        Logical NOT
+        """
         op = self.__cmd_stack_pop()
         res = ~op
         self.__update_flags(res)
@@ -190,6 +227,9 @@ class Assembler():
         )
     
     def __or(self):
+        """
+        Logical OR
+        """
         op1 = self.__cmd_stack_pop()
         op2 = self.__cmd_stack_pop()
         res = op1 | op2
@@ -201,6 +241,9 @@ class Assembler():
         )
 
     def __and(self):
+        """
+        Logical AND
+        """
         op1 = self.__cmd_stack_pop()
         op2 = self.__cmd_stack_pop()
         res = op1 & op2
@@ -212,6 +255,9 @@ class Assembler():
         )
 
     def __xor(self):
+        """
+        Logical XOR
+        """
         op1 = self.__cmd_stack_pop()
         op2 = self.__cmd_stack_pop()
         res = op1 ^ op2
@@ -223,6 +269,9 @@ class Assembler():
         )
 
     def __nor(self):
+        """
+        Logical NOR
+        """
         op1 = self.__cmd_stack_pop()
         op2 = self.__cmd_stack_pop()
         res = ~(op1 | op2)
@@ -234,6 +283,9 @@ class Assembler():
         )
 
     def __shl(self):
+        """
+        Logical left shift by 1
+        """
         op = self.__cmd_stack_pop()
         res = op << 1
         self.__update_flags(res)
@@ -244,6 +296,9 @@ class Assembler():
         )
 
     def __shr(self):
+        """
+        Logical right shift by 1
+        """
         op = self.__cmd_stack_pop()
         res = op >> 1
         self.__update_flags(res)
@@ -254,11 +309,58 @@ class Assembler():
         )
 
     def __jmp(self, address):
+        """
+        Go to the address obtained from the label
+        """
+        # In this case, address -- line of asm program
         self.__R['PC'] = address
 
     def __jc(self, address):
-        self.__R['PC'] = address
+        """
+        Go to the address if carry flag
+        """
+        # In this case, address -- line of asm program
+        if self.__flags['C']:
+            self.__R['PC'] = address
+        else:
+            self.__R['PC'] += 1
 
     def __jz(self, address):
-        self.__R['PC'] = address
+        """
+        Go to the address if zero flag
+        """
+        # In this case, address -- line of asm program
+        if self.__flags['Z']:
+            self.__R['PC'] = address
+        else:
+            self.__R['PC'] += 1
 
+    def __jp(self, address):
+        """
+        Go to the address if parity flag (even number)
+        """
+        # In this case, address -- line of asm program
+        if self.__flags['C']:
+            self.__R['PC'] = address
+        else:
+            self.__R['PC'] += 1
+
+    def __js(self, address):
+        """
+        Go to the address if sign flag (number < 0)
+        """
+        # In this case, address -- line of asm program
+        if self.__flags['Z']:
+            self.__R['PC'] = address
+        else:
+            self.__R['PC'] += 1
+
+    def __jo(self, address):
+        """
+        Go to the address if overflow flag
+        """
+        # In this case, address -- line of asm program
+        if self.__flags['Z']:
+            self.__R['PC'] = address
+        else:
+            self.__R['PC'] += 1
--- compiler/constants.py
+++ compiler/constants.py
@@ -3,6 +3,8 @@ CMDCODE_LENGTH = 8
 LITERAL_LENGTH = 16
 ADDRESS_LENGTH = 8
 REGISTER_LENGTH = 3
+MEMORY_RATIO = 4
+FIRST_DATA_ADDRESS = (2**ADDRESS_LENGTH) / MEMORY_RATIO
 
 CMD_CODES = {
     'ADD' : '1',
@@ -14,10 +16,13 @@ CMD_CODES = {
     'OR'  : '7',
     'AND' : '8',
     'XOR' : '9',
-    'NOR' : 'A',
-    'SHL' : 'B',
-    'SHR' : 'C',
-    'JMP' : 'D',
-    'JC'  : 'E',
-    'JZ'  : 'F',
+    'NOR' : '10',
+    'SHL' : '11',
+    'SHR' : '12',
+    'JMP' : '13',
+    'JC'  : '14',
+    'JZ'  : '15',
+    'ZP'  : '16',
+    'JS'  : '17',
+    'JO'  : '18',
 }
